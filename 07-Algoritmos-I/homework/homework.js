2e4c6b788276670fb177ecc309956722d5304a39
juaniarrigoni <juani.arrigoni@gmail.comgit config --global user.name juaniarrigonigit initgit config --global user.email juani.arrigoni@gmail.com>
2022-06-01 09:26:41 -0300

    HW

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 // No cambies los nombres de las funciones.
 
 function factorear(num) {
@@ -6,7 +6,18 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  var arr = [1];
+  var divisor = 2;
+  while (num !== 1) {
+    if (num % divisor === 0) {
+      arr.push(divisor);
+      num = num / divisor;
+      factorear(num);
+    } else {
+      divisor++;
+    }
+  }
+  return arr;
 }
 
 function bubbleSort(array) {
@@ -14,27 +25,56 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  var cambio = true;
+  while (cambio) {
+    cambio = false;
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        let aux = array[i + 1];
+        array[i + 1] = array[i];
+        array[i] = aux;
+        cambio = true;
+      }
+    }
+  }
+  return array;
 }
-
 
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 1; i < array.length; i++) {
+    var x = i - 1;
+    var aux = array[i]; // elemento a comparar y que sinertamos
+    while (x >= 0 && array[x] > aux) {
+      array[x + 1] = array[x];
+      x--;
+    }
+    array[x + 1] = aux;
+  }
+  return array;
 }
-
 
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    let aux = array[i];
+    array[i] = array[min];
+    array[min] = aux;
+  }
+  return array;
 }
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
